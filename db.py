9d38969b28132e85b33adadbea5aac996283c1d0
Asyn-c <asim.aliew1967@gmail.com>
2025-06-25 22:29:38 +0200

    Added working database and scraper logic

--- db.py
+++ db.py
@@ -1,14 +1,17 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def save_ad_to_db(title, price, link):
     conn = mysql.connector.connect(
-        host="127.0.0.1",
-        port=3306,
-        user="root",
-        password="root",
-        database="auto_aggregator_db"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
-
     cursor = conn.cursor()
     cursor.execute("""
         INSERT INTO ads (title, price, link)
@@ -17,17 +20,16 @@ def save_ad_to_db(title, price, link):
     conn.commit()
     conn.close()
 
-
 def get_all_ads():
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="auto_aggregator_db"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
     cursor = conn.cursor()
     cursor.execute("SELECT title, price, link FROM ads")
     results = cursor.fetchall()
     conn.close()
     return results
-
